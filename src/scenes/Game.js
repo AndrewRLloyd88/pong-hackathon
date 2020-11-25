738be166661084b195eb24a92404237b5898ddfd
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-11-25 10:50:52 -0800

    add working ai

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,16 +1,15 @@
 import Phaser from "phaser";
 import mp3 from "../assets/Orbital Colossus.mp3";
-import background from "../assets/scifi_platform_BG1.jpg";
-import ball from "../assets/ball.png";
 import paddle1 from "../assets/paddle1.png";
 import paddle2 from "../assets/paddle2.png";
 import bg from "../assets/bg.jpg";
-import tiles from "../assets/scifi_platformTiles_32x32.png";
-import star from "../assets/star.png";
+import ballimg from "../assets/ball.png";
 import { accelerate, decelerate } from "../utils";
 
 let paddleP1;
+let paddleP2;
 let cursors;
+let ball;
 
 export default new Phaser.Class({
   Extends: Phaser.Scene,
@@ -21,47 +20,64 @@ export default new Phaser.Class({
   preload: function preload() {
     this.load.image("bg", bg);
     this.load.image("paddle1", paddle1);
-
-    this.load.image("star", star);
+    this.load.image("paddle2", paddle2);
+    this.load.image("ballimg", ballimg);
   },
   create: function create() {
     this.add.image(400, 300, "bg");
     this.add.image("paddle1", paddle1);
+    this.add.image("paddle2", paddle2);
+    this.add.image("ballimg", ballimg);
 
-    const stars = this.physics.add.group({
-      key: "star",
-      repeat: 0,
-      setScale: { x: 0.2, y: 0.2 },
-      setXY: { x: 400, y: 300 },
-    });
+    ball = this.physics.add.image(300, 300, "ballimg");
 
-    stars.children.iterate(function (child) {
-      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-      child.setVelocityX(150 - Math.random() * 300);
-      child.setVelocityY(150 - Math.random() * 300);
-      child.setBounce(1, 1);
-      child.setCollideWorldBounds(true);
-    });
+    ball.setBounceY(Phaser.Math.FloatBetween(0.5, 0.8));
+    ball.setVelocityX(150 - Math.random() * 300);
+    ball.setVelocityY(150 - Math.random() * 300);
+    ball.setBounce(1, 1);
+    ball.setCollideWorldBounds(true);
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    paddleP1 = this.physics.add.image(700, 300, "paddle1");
+    //declaring our P1 controlled paddle at X 10 and Y 300 (half of our canvas size)
+    paddleP1 = this.physics.add.image(10, 300, "paddle1");
 
-    const processCollision = (paddleP1, star) => {
+    //declaring our P2 controlled paddle at X 790 (right side of canvas) and Y 300 (half of our canvas size)
+    paddleP2 = this.physics.add.image(790, 300, "paddle2");
+
+    const processCollision = (paddleP2, ball) => {
+      ball.setBounceY(Phaser.Math.FloatBetween(0.5, 0.8));
+      ball.setBounce(1, 1);
       // star.destroy();
-      const starsLeft = stars.countActive();
-      if (starsLeft === 0) {
-        this.scene.start("winscreen");
-      }
     };
 
-    this.physics.add.collider(stars, paddleP1, processCollision, null, this);
+    //adding collision detection for paddleP1 and the star
+    this.physics.add.collider(ball, paddleP1, processCollision, null, this);
 
+    //adding collision detection for paddleP2 and the star
+    this.physics.add.collider(ball, paddleP2, processCollision, null, this);
+
+    //setting paddleP1 bounce
     paddleP1.setBounce(1, 1);
+    //set worldcollisionbounds for P1 so it can't leave the playing field
     paddleP1.setCollideWorldBounds(true);
+
+    //setting paddleP2 bounce
+    paddleP2.setBounce(1, 1);
+    //set worldcollisionbounds for P1 so it can't leave the playing field
+    paddleP2.setCollideWorldBounds(true);
   },
   update: function () {
+    let targetVelocity = ball.body.velocity.y;
     const { velocity } = paddleP1.body;
+
+    // ball.x += 5;
+
+    paddleP2.body.setVelocity(ball.body.velocity.y);
+    paddleP2.body.velocity.x = -ball.body.velocity.x * 0.5;
+    paddleP2.body.maxVelocity.y = 250;
+
+    ball.setBounce(1, 1);
 
     if (cursors.space.isDown) {
       const x = decelerate(velocity.x);
